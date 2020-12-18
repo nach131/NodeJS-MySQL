801515073d53209e0f0fa7cd119a80c02f7d795d
nacho <nacho.mota@gmail.com>
2020-12-18 20:58:05 +0100

    INICIO

---
+++ src/database.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+const { promisify } = require('util')
+const { database } = require('./keys')
+
+const pool = mysql.createPool(database);
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Se cerro la conexion con la base de datos')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('DEMASIADAS CONEXIONES A LA BD')
+    }
+    if (err.code === 'ECONNREFUSE') {
+      console.error('LA CONEXION FUE RECHAZADA')
+    }
+  }
+  if (connection) connection.release();
+  console.log('Conectado a DB');
+  return;
+})
+
+pool.query = promisify(pool.query);
+
+module.exports = pool;
---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+// inicializaciones
+
+const app = express();
+
+// settings
+app.set("port", process.env.PORT || 4000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layaouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    helpers: require("./lib/handlebars"),
+  })
+);
+app.set("view engine", ".hbs");
+
+// Midlewares
+// Funciones que se ejecutan cada vez que el cliente envia una peticion
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Variables Globales
+
+app.use((req, res, next) => {
+  next();
+});
+
+// Routes
+app.use(require("./routes"));
+app.use(require("./routes/authentication"));
+app.use("links", require("./routes/links"));
+
+// Public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Starting the server
+app.listen(app.get("port"), () => {
+  console.log("Servidor esta en el puerto", app.get("port"));
+});
